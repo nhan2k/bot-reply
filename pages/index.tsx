f324d51b2af1f777dbd628408da38d78441b2479
nhan2k <nhanrooney112@gmail.com>
2022-12-30 16:09:23 +0700

    remove comment code

--- pages/index.tsx
+++ pages/index.tsx
@@ -175,14 +175,4 @@ const Home: React.FC<IHome> = () => {
   );
 };
 
-// // This gets called on every request
-// export async function getServerSideProps() {
-//   // Fetch data from external API
-//   const res = await fetch(`https://jsonplaceholder.typicode.com/todos`);
-//   const data = await res.json();
-
-//   // Pass data to the page via props
-//   return { props: { data } };
-// }
-
 export default Home;
